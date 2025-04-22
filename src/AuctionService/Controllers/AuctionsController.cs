c6aad1e153ac6d9cf27af25db99498d1ab4880ea
unknown <venkat,jkb@gmail.com>
2025-04-22 11:48:32 +0530

    first commit of AuctionService

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,92 @@
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuctionsController(IAuctionRepository repo, IMapper mapper)
+    : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string? date)
+    {
+        return await repo.GetAuctionsAsync(date);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await repo.GetAuctionByIdAsync(id);
+
+        if (auction == null) return NotFound();
+
+        return auction;
+    }
+
+    // [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
+    {
+        var auction = mapper.Map<Auction>(createAuctionDto);
+
+        auction.Seller = User.Identity?.Name ?? "Unknown user";
+
+        repo.AddAuction(auction);
+
+        var newAuction = mapper.Map<AuctionDto>(auction);
+
+
+        var result = await repo.SaveChangesAsync();
+
+        if (!result) return BadRequest("Could not save changes to DB");
+
+        return CreatedAtAction(nameof(GetAuctionById),
+            new { Id = auction.Id }, newAuction);
+    }
+
+    // [Authorize]
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await repo.GetAuctionEntityById(id);
+
+        if (auction == null) return NotFound();
+
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+
+        var result = await repo.SaveChangesAsync();
+
+        if (result) return Ok();
+
+        return BadRequest("Problem saving changes");
+    }
+
+    // [Authorize]
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await repo.GetAuctionEntityById(id);
+
+        if (auction == null) return NotFound();
+
+
+        repo.RemoveAuction(auction);
+
+
+        var result = await repo.SaveChangesAsync();
+
+        if (!result) return BadRequest("Could not update DB");
+
+        return Ok();
+    }
+}
---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,49 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (DbUpdateException ex)
+{
+    Console.WriteLine($"Error updating database: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unexpected error: {ex.Message}");
+}
+
+app.Run();
